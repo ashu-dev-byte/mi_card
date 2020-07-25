af7d676bfbe7eb934a8d252b16ac3c5b665549c4
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-07-26 03:49:57 +0530

    Basic UI top part done

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,36 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage("images/ashu.jpg"),
+                ),
+                Text(
+                  "Ashutosh Anand",
+                  style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Fullstack Developer",
+                  style: TextStyle(
+                      fontFamily: "Source Sans Pro",
+                      fontSize: 37.5,
+                      letterSpacing: 0.3,
+                      color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+        debugShowCheckedModeBanner: false),
   );
 }
