0a30c37806d8634f129feb576d568dd60a492934
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-07-26 04:06:36 +0530

    Added phone and email cards

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,50 @@ void main() {
                       letterSpacing: 0.3,
                       color: Colors.white),
                 ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.all(20.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "+91 96930 48167",
+                        style: TextStyle(
+                            color: Colors.teal[600],
+                            fontFamily: "Source Sans Pro",
+                            fontSize: 20.0),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.all(20.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        "ashutosh.anand_cs17@gla.ac.in",
+                        style: TextStyle(
+                            color: Colors.teal[600],
+                            fontFamily: "Source Sans Pro",
+                            fontSize: 17.5),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
